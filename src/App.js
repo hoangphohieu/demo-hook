9b3e71acba804712cf11f81c1dd2de1abcb1c07f
hoangphohieu <Hoangphohieu1@gmail.com>
2020-06-19 23:22:45 +0700

    hieu

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{useState} from 'react';
 import './App.css';
-
+import Child1 from './child/Child1';
 function App() {
+  let a = 1;
+  const [Num, setNum] = useState(0);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Child1 item={a} />
+      <button  onClick={()=>setNum(Num+1)}>{Num}</button>
     </div>
   );
 }
---
+++ src/child/Child1.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Child1(props) {
+      console.log(props.item);
+      
+      return (
+            <div>
+                  child1
+            </div>
+      );
+}
+
+export default Child1;

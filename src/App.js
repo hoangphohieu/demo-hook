ee69ce09f960b55adb4e9aa986d6a6b50a5e8635
hoangphohieu <Hoangphohieu1@gmail.com>
2020-07-01 23:32:45 +0700

    hieu dz

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 
 
 function App() {
@@ -9,14 +9,32 @@ function App() {
     })
 
   }
+  const inputEl = useRef(null);
+ 
   const [Toado, setToado] = useState({ x: 0, y: 0 });
 
-  return (
-    <div style={{ height: '100%' }} onMouseMove={handleMouseMove}>
-      <h1>Move the mouse around!</h1>
-      <p>The current mouse position is ({Toado.x}, {Toado.y})</p>
-    </div>
+  const [Stype, setStype] = useState({
+    "width": 100,
+    "height": 100,
+    "background": "purple",
+    "position": "absolute"
+  });
+  useEffect(() => {
+    let Position = inputEl.current.getBoundingClientRect();
+    setStype({ ...Stype, top: Toado.y, left: Toado.x })
+    console.log(Position);
 
+
+  },[Toado])
+  return (
+    <React.Fragment>
+      <div style={{ height: '800px', width: "1500px", "position": "relative" }} onMouseMove={handleMouseMove}>
+    
+        <p style={{position:"absolute",top:0}}>The current mouse position is ({Toado.x}, {Toado.y})</p>
+        <div style={Stype} ref={inputEl}></div>
+      </div>
+
+    </React.Fragment>
   );
 }
 

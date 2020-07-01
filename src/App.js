205d20e3318ee04343a5fdea177cac27e0c255b1
Hoangphohieu <hoangphohieu1@gmail.com>
2020-07-01 18:17:15 +0700

    ff

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import React, { useState } from 'react';
-import './App.css';
-import Child1 from './child/Child1';
-const MyContext = React.createContext();
+
 
 function App() {
-  const [Persion, setPersion] = useState({
-    name: 'Dan',
-    age: 100,
-    cool: true
-  });
+  let handleMouseMove = (event) => {
+    setToado({
+      x: event.clientX,
+      y: event.clientY
+    })
+
+  }
+  const [Toado, setToado] = useState({ x: 0, y: 0 });
+
   return (
-    <div className="App">
-      <MyContext.Provider value={Persion}>
-        <Child1 />
-
-      </MyContext.Provider>
-
-
+    <div style={{ height: '100%' }} onMouseMove={handleMouseMove}>
+      <h1>Move the mouse around!</h1>
+      <p>The current mouse position is ({Toado.x}, {Toado.y})</p>
     </div>
+
   );
 }
 
--- src/child/Child1.js
+++ src/child/Child1.js
@@ -1,34 +1,19 @@
-import React, { useEffect, useState, useRef,MyContext } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
+import { ThemeContext } from './ThemeContext';
 
 function Clock() {
-      const [timeString, setTimeString] = useState(null);
-      const intervalRef = useRef(null);
-      useEffect(() => {
-            intervalRef.current = setInterval(() => {
-                  const now = new Date();
-                  const hours = `0${now.getHours()}`.slice(-2);
-                  const minutes = `0${now.getMinutes()}`.slice(-2);
-                  const seconds = `0${now.getSeconds()}`.slice(-2);
-                  const currentTimeString = `${hours}:${minutes}:${seconds}`;
-                  setTimeString(currentTimeString);
-                  console.log('Component Did Mount');
-            }, 1000);
-            return () => {
-                  clearInterval(intervalRef.current);
-                  console.log('Component Will Unmount');
-            };
-      }, []);
       return (
-            <MyContext.Consumer>
-                  {(context) => (
-                        <React.Fragment>
-                             
-                              <div style={{ fontSize: '48px' }}>{context}</div>
-                        </React.Fragment>
-                  )}
-            </MyContext.Consumer>
+            <ThemeContext.Consumer>
+                  {
+                        ( themes ) => (
+                              <React.Fragment>
+                                    <div style={{ fontSize: '48px' }}>ct: {themes.light.foreground}</div>
+                              </React.Fragment>
+                        )}
+            </ThemeContext.Consumer>
 
       );
 }
+
 
 export default Clock;
